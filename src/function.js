1df5b737bcd99893bec0743baf75561d1e8454da
vcheungg <vivianncheungg@gmail.com>
2025-04-24 10:12:59 -0700

    Added time/date function and troubleshooted api calling

--- src/function.js
+++ src/function.js
@@ -9,6 +9,8 @@ function updateWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeed = document.querySelector("#wind-speed");
   let wind = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   iconElement.innerHTML = `<img
                 src="${response.data.condition.icon_url}"
@@ -16,11 +18,32 @@ function updateWeather(response) {
               />`;
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formattedDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   feelTemperature.innerHTML = Math.round(feel);
   humidityElement.innerHTML = response.data.temperature.humidity;
   windSpeed.innerHTML = Math.round(wind);
   temperatureElement.innerHTML = Math.round(temp);
+}
+
+function formattedDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 function citySearch(city) {
   let apiKey = "047t2173e3a39c66942c701baf3a6of5";
